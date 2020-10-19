9ed4d59256df09cdfb7d7310f0fc9743278e8b86
Lang Yuan <peacocker@163.com>
2020-10-19 22:31:16 +0800

    start to merge from the end

--- sort_merge.py
+++ sort_merge.py
@@ -38,7 +38,7 @@
 from typing import List
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def mergev1(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
@@ -67,6 +67,26 @@ class Solution:
             index2 += 1
             remain2 -= 1
 
+    # from end to start
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p = m + n - 1
+        p1 = m - 1
+        p2 = n - 1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p -= 1
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p -= 1
+                p2 -= 1
+                
+        # add missing elements from nums2
+        nums1[:p2 + 1] = nums2[:p2 + 1]
 
 if __name__ == "__main__":
     # expect: True
