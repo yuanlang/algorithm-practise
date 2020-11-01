c39da90ed76287c7028861692164169e9965dfa1
Lang Yuan <peacocker@163.com>
2020-11-01 22:40:15 +0800

    lru cache OrderedDict version

--- design/lru_cache.py
+++ design/lru_cache.py
@@ -23,72 +23,69 @@
 # cache.get(3);       // 返回  3
 # cache.get(4);       // 返回  4
 
+from collections import OrderedDict
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         #存储容量大小
         self.capacity = capacity
         #存储KV对
-        self.kv_list = dict()
-        #存储最后一次访问的顺序
-        self.kv_order = dict()
-        #顺序，get或者put时都会增加1
-        self.order = 0
+        self.kv_dict = OrderedDict()
         #当前缓存中的条数
         self.amount = 0
 
     def get(self, key: int) -> int:
         #如果key不存在，则返回-1
-        if key not in self.kv_list:
+        if key not in self.kv_dict:
             return -1
 
         #key存在，则返回值，并更新当前order值为最新值
-        ret = self.kv_list[key]
-        self.kv_order[key] = self.order
-
-        #增加order值
-        self.order += 1
+        ret = self.kv_dict[key]
+        #更新词典中顺序
+        value = self.kv_dict.pop(key)
+        self.kv_dict[key] = value
 
         return ret
 
     def put(self, key: int, value: int) -> None:
         #已经存在，直接更新值和最后一次访问时间
-        if key in self.kv_list:
-            self.kv_list[key] = value
-            self.kv_order[key] = self.order
-            self.order += 1
+        if key in self.kv_dict:
+            self.kv_dict.pop(key)
+            self.kv_dict[key] = value
         #不存在，
         #  判断缓存大小，还有缓存，则存入当前key val
         #  无缓存，需删除最近最少使用的数据值，再写入
         else:
             if self.amount == self.capacity:
                 #找到需要退出缓存的key
-                min_key = float('inf')
-                min_order = float('inf')
-                for k, v in self.kv_order.items():
-                    if min_order > v:
-                        min_key = k
-                        min_order = v
-                del(self.kv_list[min_key])
-                del(self.kv_order[min_key])
+                self.kv_dict.popitem(last=False)
                 self.amount -= 1
-            self.kv_list[key] = value
-            self.kv_order[key] = self.order
-            self.order += 1
+            self.kv_dict[key] = value
             self.amount += 1
+        # print(self.kv_dict)
 
 if __name__ == "__main__":
     # Your LRUCache object will be instantiated and called as such:
+    # capacity = 2
+    # cache = LRUCache(capacity)
+
+    # cache.put(1, 1)
+    # cache.put(2, 2)
+    # print(cache.get(1)) # 返回  1
+    # cache.put(3, 3) # 该操作会使得密钥 2 作废
+    # print(cache.get(2)) # 返回 - 1 (未找到)
+    # cache.put(4, 4) # 该操作会使得密钥 1 作废
+    # print(cache.get(1)) # 返回 - 1 (未找到)
+    # print(cache.get(3)) # 返回  3
+    # print(cache.get(4)) # 返回  4
+
     capacity = 2
     cache = LRUCache(capacity)
 
+    cache.put(2, 1)
     cache.put(1, 1)
-    cache.put(2, 2)
-    print(cache.get(1)) # 返回  1
-    cache.put(3, 3) # 该操作会使得密钥 2 作废
-    print(cache.get(2)) # 返回 - 1 (未找到)
-    cache.put(4, 4) # 该操作会使得密钥 1 作废
-    print(cache.get(1)) # 返回 - 1 (未找到)
-    print(cache.get(3)) # 返回  3
-    print(cache.get(4)) # 返回  4
-
+    cache.put(2, 3)
+    cache.put(4, 1)
+    print(cache.get(1))
+    print(cache.get(2))
